9e45fd84d5b34774f36c8fe3449af30e533f7864
aastikn <aastiksimple@gmail.com>
2025-09-15 21:39:26 +0530

    feat:init

--- xeno-crm/backend/src/main/java/com/aastikn/crm_backend_api/repository/SegmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aastikn.crm_backend_api.repository;
-
-import com.aastikn.crm_backend_api.entity.Segment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SegmentRepository extends JpaRepository<Segment, Long> {
-}
