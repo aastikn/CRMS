d480d4a2d0654e12d0d9b4e3dde4a13dd6382de4
aastikn <aastiksimple@gmail.com>
2025-09-16 11:32:43 +0530

    feat:working

--- xeno-crm/frontend/src/app/campaigns/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteGuard } from '../../components/RouteGuard';
-
-export default function CampaignsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <RouteGuard>{children}</RouteGuard>;
-}
--- xeno-crm/frontend/src/contexts/AuthContext.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-import { createContext } from 'react';
-
-// Create a context with a default value
-export const AuthContext = createContext({});
